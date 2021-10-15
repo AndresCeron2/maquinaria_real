13bab361406cb4a12ed398a93518c975dd306543
AndresCeron2 <91512978+AndresCeron2@users.noreply.github.com>
2021-10-15 18:24:16 -0500

    Create ReservationController.java

---
+++ src/main/java/com/maquinaria/maquinaria2/app/controllers/ReservationController.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.maquinaria.maquinaria2.app.controllers;
+
+import com.maquinaria.maquinaria2.app.entities.Reservation;
+import com.maquinaria.maquinaria2.app.services.ReservationService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author aldon
+ */
+@RestController
+@RequestMapping("Reservation")
+public class ReservationController {
+   
+    @Autowired
+    private ReservationService service;
+	    
+    /**
+     * GET
+     * @return 
+     */
+    @GetMapping("/all")
+    public List<Reservation> getReservations(){
+        return service.getAll();
+    }
+	    
+    /**
+     * POST
+     * @param category
+     * @return 
+     */
+    @PostMapping("/save")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Reservation save(@RequestBody Reservation category) {
+        return service.save(category);
+    }
+    
+    /**
+     * PUT
+     * @param category
+     * @return 
+     */
+    @PutMapping("/update")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Reservation update(@RequestBody Reservation category) {
+        return service.update(category);
+    }
+    
+    /**
+     * DELETE
+     * @param categoryId
+     * @return 
+     */
+    @DeleteMapping("/{id}")
+    public boolean delete(@PathVariable("id") int categoryId) {
+        return service.deleteReservation(categoryId);
+    }
+    
+}
