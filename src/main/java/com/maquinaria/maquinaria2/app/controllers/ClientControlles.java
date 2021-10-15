5614df8470ccf102fe6f8023c4d8e24365aa9be1
AndresCeron2 <91512978+AndresCeron2@users.noreply.github.com>
2021-10-15 14:48:48 -0500

    Update ClientControlles.java

--- src/main/java/com/maquinaria/maquinaria2/app/controllers/ClientControlles.java
+++ src/main/java/com/maquinaria/maquinaria2/app/controllers/ClientControlles.java
@@ -9,13 +9,16 @@ import com.maquinaria.maquinaria2.app.entities.Client;
 import com.maquinaria.maquinaria2.app.services.ClientService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 /**
  *
@@ -23,31 +26,51 @@ import org.springframework.web.bind.annotation.RestController;
  */
 
 @RestController
-@RequestMapping("Clients")
+@RequestMapping("Client")
 public class ClientControlles {
+   
     @Autowired
     private ClientService service;
     
+    /**
+     * GET
+     * @return 
+     */
     @GetMapping("/all")
-    public List<Client> findAllClient(){
-        return service.getClient();
+    public List<Client> getClients(){
+        return service.getAll();
     }
     
+    /**
+     * POST
+     * @param client
+     * @return 
+     */
     @PostMapping("/save")
-    public ResponseEntity addClient(@RequestBody Client client){
-        service.saveClient(client);
-        return ResponseEntity.status(201).build();
+    @ResponseStatus(HttpStatus.CREATED)
+    public Client save(@RequestBody Client client) {
+        return service.save(client);
     }
     
+    /**
+     * PUT
+     * @param client
+     * @return 
+     */
     @PutMapping("/update")
-    public ResponseEntity updateClient(@RequestBody Client client){
-        service.updateClient(client);
-        return ResponseEntity.status(201).build();
+    @ResponseStatus(HttpStatus.CREATED)
+    public Client update(@RequestBody Client client) {
+        return service.update(client);
     }
     
-    @DeleteMapping("/delete")
-    public ResponseEntity deleteClient(@RequestBody Client client){
-        service.deleteClient(client.getId());
-        return ResponseEntity.status(204).build();
+    /**
+     * DELETE
+     * @param clientId
+     * @return 
+     */
+    @DeleteMapping("/{id}")
+    public boolean delete(@PathVariable("id") int clientId) {
+        return service.deleteClient(clientId);
     }
+    
 }
