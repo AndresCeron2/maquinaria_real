7a33c60227d2716a11a0c023ec55c4b1e1fe7034
AndresCeron2 <91512978+AndresCeron2@users.noreply.github.com>
2021-10-15 16:37:48 -0500

    Update ClientRepositorie.java

--- src/main/java/com/maquinaria/maquinaria2/app/repositories1/ClientRepositorie.java
+++ src/main/java/com/maquinaria/maquinaria2/app/repositories1/ClientRepositorie.java
@@ -8,6 +8,7 @@ package com.maquinaria.maquinaria2.app.repositories1;
 import com.maquinaria.maquinaria2.app.repositories.crud.ClientCRUDRepository;
 import java.util.List;
 import com.maquinaria.maquinaria2.app.entities.Client;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
@@ -26,6 +27,32 @@ public class ClientRepositorie {
      */
     public List<Client> getAll(){
         return (List<Client>) clientCrudRepository.findAll();
+    }
+    
+    /**
+     * Insert
+     * @param client
+     * @return 
+     */
+    public Client save(Client client){
+       return clientCrudRepository.save(client);
+    } 
+    
+    /**
+     * Buscar registro
+     * @param clientId
+     * @return
+     */
+    public Optional<Client> getClient(int clientId){
+        return clientCrudRepository.findById(clientId);
+    }
+    
+    /**
+     * Delete
+     * @param client
+     */
+    public void delete(Client client){
+        clientCrudRepository.delete(client);
     }
 }
 
